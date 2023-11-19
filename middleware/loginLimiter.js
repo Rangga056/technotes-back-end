56e76ff9368fe7dc6641a7da17a144629fc657e3
Rangga <muhammadrangga056@gmail.com>
2023-11-19 17:06:37 +0700

    Finalize backend api

--- middleware/loginLimiter.js
+++ middleware/loginLimiter.js
@@ -3,9 +3,10 @@ const { logEvents } = require("./logger");
 
 const loginLimiter = rateLimit({
   windowMs: 60 * 1000, // 1 minute
-  max: 5, // Limit each IP to 5 login requests per 'windows' per minute
+  max: 5, // Limit each IP to 5 login requests per `window` per minute
   message: {
-    message: "To many login attempts from this IP, please try again later ",
+    message:
+      "Too many login attempts from this IP, please try again after a 60 second pause",
   },
   handler: (req, res, next, options) => {
     logEvents(
@@ -14,8 +15,8 @@ const loginLimiter = rateLimit({
     );
     res.status(options.statusCode).send(options.message);
   },
-  standardHeaders: true, // return rate limit info in the 'RateLimit-*' headers
-  legacyHeaders: false, // Disable the 'X-RateLimit-*' headers
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
 });
 
 module.exports = loginLimiter;
